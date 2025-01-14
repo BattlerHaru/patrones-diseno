c8d64ce9b002e748baca0dd04c9dbe6047b35b2d
BattlerHaru <kronk.sdance@gmail.com>
2025-01-14 13:04:41 -0600

    :package: feat: add builder pattern example in 01-creacionales/01-builder.ts

--- 01-creacionales/01-builder.ts
+++ 01-creacionales/01-builder.ts
@@ -12,3 +12,81 @@
  *
  * https://refactoring.guru/es/design-patterns/builder
  */
+
+// * Ejemplo patron Builder  
+
+import { COLORS } from "../helpers/colors.ts";
+
+class Computer {
+    public cpu: string = "cpu - not defined";
+    public ram: string = "ram - not defined";
+    public storage: string = "storage - not defined";
+    public gpu?: string = "gpu - not defined";
+
+    displayConfiguration() {
+        console.log( `Configuración de la computadora
+            CPU: ${ this.cpu }
+            RAM: ${ this.ram }
+            Almacenamiento: ${ this.storage }
+            GPU: ${ this.gpu ?? "No cuenta con GPU" }
+            `);
+    }
+
+}
+
+class ComputerBuilder {
+    private computer: Computer;
+
+    constructor() {
+        this.computer = new Computer();
+    }
+
+    setCPU( cpu: string ): ComputerBuilder {
+        this.computer.cpu = cpu;
+        return this;
+    }
+
+    setRAM( ram: string ): ComputerBuilder {
+        this.computer.ram = ram;
+        return this;
+    }
+
+    setStorage( storage: string ): ComputerBuilder {
+        this.computer.storage = storage;
+        return this;
+    }
+
+    setGPU( gpu: string ): ComputerBuilder {
+        this.computer.gpu = gpu;
+        return this;
+    }
+
+    build() {
+        return this.computer;
+    }
+
+}
+
+function main() {
+    // const basicComputer = new ComputerBuilder()
+    //     .setCPU( "Intel Core 2 Dúo" )
+    //     .setRAM( "4GB" )
+    //     .setStorage( '256GB' )
+    //     .build();
+
+    // console.log( "%cComputadora Básica:", COLORS.blue );
+    // basicComputer.displayConfiguration();
+
+    const gamerComputer = new ComputerBuilder()
+        .setCPU( "Intel Core i9 14th 14900k" )
+        .setRAM( "32GB" )
+        .setStorage( '2TB M2' )
+        .setGPU( 'Nvidia GeForce RTX 4090' )
+        .build();
+
+    console.log( "%cComputadora Gamer:", COLORS.purple );
+    gamerComputer.displayConfiguration();
+
+}
+
+main();
