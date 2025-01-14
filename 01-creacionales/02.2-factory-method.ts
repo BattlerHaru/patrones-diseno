19f629c5b07510cb63b889c0ff9e1ff9b9715169
BattlerHaru <kronk.sdance@gmail.com>
2025-01-14 13:43:42 -0600

    :package: feat: add factory method pattern task in 01-creacionales/02.2-factory-method.ts

--- 01-creacionales/02.2-factory-method.ts
+++ 01-creacionales/02.2-factory-method.ts
@@ -16,13 +16,15 @@
  * 	!Descripción:
   1.	Completen las clases SalesReport e InventoryReport para implementar 
       la interfaz Report, generando el contenido de cada reporte en el método generate.
-	  
+    
   2.	Implementen las clases SalesReportFactory e InventoryReportFactory 
       para crear instancias de SalesReport y InventoryReport, respectivamente.
 
-	3.	Prueben el programa generando diferentes tipos de reportes usando
+  3.	Prueben el programa generando diferentes tipos de reportes usando
       el prompt para seleccionar el tipo de reporte.
  */
+
+//! Solución  
 
 import { COLORS } from '../helpers/colors.ts';
 
@@ -35,19 +37,21 @@ interface Report {
 // Implementar SalesReport e InventoryReport
 
 class SalesReport implements Report {
-  // TODO: implementar el método e imprimir en consola:
-  // 'Generando reporte de ventas...'
+  generate(): void {
+    console.log( "%cGenerando el reporte de ventas", COLORS.green );
+  }
 }
 
 class InventoryReport implements Report {
-  // TODO: implementar el método e imprimir en consola:
-  // 'Generando reporte de inventario...'
+  generate(): void {
+    console.log( "%cGenerando el reporte de inventario", COLORS.orange );
+  }
 }
 
 // 3. Clase Base ReportFactory con el Método Factory
 
 abstract class ReportFactory {
-  abstract createReport(): Report;
+  protected abstract createReport(): Report;
 
   generateReport(): void {
     const report = this.createReport();
@@ -59,13 +63,13 @@ abstract class ReportFactory {
 
 class SalesReportFactory extends ReportFactory {
   createReport(): Report {
-    throw new Error('Method not implemented.');
+    return new SalesReport;
   }
 }
 
 class InventoryReportFactory extends ReportFactory {
   createReport(): Report {
-    throw new Error('Method not implemented.');
+    return new InventoryReport;
   }
 }
 
@@ -75,11 +79,10 @@ function main() {
   let reportFactory: ReportFactory;
 
   const reportType = prompt(
-    '¿Qué tipo de reporte deseas? %c(sales/inventory)',
-    COLORS.red
+    '¿Qué tipo de reporte deseas? %c(sales/inventory)'
   );
 
-  if (reportType === 'sales') {
+  if ( reportType === 'sales' ) {
     reportFactory = new SalesReportFactory();
   } else {
     reportFactory = new InventoryReportFactory();
