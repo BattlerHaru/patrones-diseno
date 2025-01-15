0adbb0d43a11b0c1910536f818f4e1384a7d6be5
BattlerHaru <kronk.sdance@gmail.com>
2025-01-14 21:57:39 -0600

    :package: feat: add singleton pattern example in 01-creacionales/06-singleton.ts

--- 01-creacionales/06-singleton.ts
+++ 01-creacionales/06-singleton.ts
@@ -9,3 +9,77 @@
  *
  * https://refactoring.guru/es/design-patterns/singleton
  */
+
+
+// * Ejemplo patron Singleton  
+
+import { COLORS } from '../helpers/colors.ts';
+
+
+class DragonBalls {
+    private static instance: DragonBalls;
+    private ballsCollected: number;
+
+    private constructor() {
+        this.ballsCollected = 0;
+    }
+
+    public static getInstance(): DragonBalls {
+        if ( !DragonBalls.instance ) {
+            DragonBalls.instance = new DragonBalls();
+            console.log( "Las esferas del dragón han sido creadas" );
+        }
+        return DragonBalls.instance;
+    }
+
+
+    collectBall(): void {
+        if ( this.ballsCollected < 7 ) {
+            this.ballsCollected++;
+            console.log( `Total de esferas del dragón encontradas: %c${ this.ballsCollected }`, COLORS.orange );
+            return;
+        }
+
+        console.log( "%cYa se han recolectado las 7 esferas del dragón!", COLORS.orange );
+        console.log( "Di las palabras mágicas para invocar a %cShen Long", COLORS.green );
+    }
+
+    summonShenLong() {
+        if ( this.ballsCollected === 7 ) {
+            console.log( "%c¡Sal de ahí Shen Long y cumple mi deseo!", COLORS.orange );
+            this.ballsCollected = 0;
+            console.log( "%cTu deseo ha sido cumplido...", COLORS.green );
+            return;
+        }
+
+        console.log( `Aún faltan ${ 7 - this.ballsCollected } esferas del dragón...` );
+    }
+}
+
+function main() {
+    const gokuDragonBalls = DragonBalls.getInstance();
+    gokuDragonBalls.collectBall();// 1
+    gokuDragonBalls.collectBall();// 2
+    gokuDragonBalls.collectBall();// 3
+    gokuDragonBalls.summonShenLong();
+
+    const vegetaDragonBalls = DragonBalls.getInstance();
+    vegetaDragonBalls.collectBall();// 4
+    vegetaDragonBalls.collectBall();// 5
+    vegetaDragonBalls.collectBall();// 6
+    vegetaDragonBalls.collectBall();// 7
+
+    gokuDragonBalls.summonShenLong();
+
+    vegetaDragonBalls.summonShenLong();
+
+
+
+
+
+}
+
+main();
+
+
+
