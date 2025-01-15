4ba1cfa3d55dd23c0a0f6079baae14ade70516c6
BattlerHaru <kronk.sdance@gmail.com>
2025-01-14 22:13:33 -0600

    :package: feat: add factory function pattern example in 01-creacionales/07-factory-function.ts

--- 01-creacionales/07-factory-function.ts
+++ 01-creacionales/07-factory-function.ts
@@ -7,3 +7,35 @@
  * * es decir, en tiempo de ejecución y no en tiempo de compilación.
  *
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+// * Ejemplo patron Factory Function 
+
+type Language = "es" | "en" | "fr";
+function createGreeter( lang: Language ) {
+    return function ( name: string ) {
+        const messages = {
+            es: `Hola, %c${ name }!`,
+            en: `Hello, %c${ name }!`,
+            fr: `Bonjour, %c${ name }!`
+        };
+
+        return console.log( messages[ lang ], COLORS.blue );
+    };
+}
+
+function main() {
+    const spanishGreeter = createGreeter( "es" );
+    const englishGreeter = createGreeter( "en" );
+    const frenchGreeter = createGreeter( "fr" );
+
+
+    spanishGreeter( "Fernando" );
+    englishGreeter( "Alice" );
+    frenchGreeter( "Pierre" );
+
+
+}
+
+main();
