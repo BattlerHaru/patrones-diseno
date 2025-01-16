86e6924e0313fd85bd0c7f8afde39e8097a69eea
BattlerHaru <kronk.sdance@gmail.com>
2025-01-15 18:16:47 -0600

    :package: feat: add Adapter pattern example use in 02-estructurales/01-adapter.ts and example api is in adapter-files/logger-adapter.ts

--- 02-estructurales/adapter-files/logger-adapter.ts
+++ 02-estructurales/adapter-files/logger-adapter.ts
@@ -1,3 +1,33 @@
 import { Logger } from 'jsr:@deno-library/logger';
 
-// TODO: Implementar el LoggerAdapter
+interface ILoggerAdapter {
+    file: string;
+
+    writeLog( msg: string ): void;
+    writeWarning( msg: string ): void;
+    writeError( msg: string ): void;
+
+}
+
+
+export class DenoLoggerAdapter implements ILoggerAdapter {
+    public file: string;
+
+    private logger = new Logger();
+
+    constructor( file: string ) {
+        this.file = file;
+    }
+
+
+    writeLog( msg: string ): void {
+        this.logger.info( `[ ${ this.file } info ] %c${ msg }` );
+    }
+    writeWarning( msg: string ): void {
+        this.logger.warn( `[ ${ this.file } warning ] %c${ msg }` );
+    }
+    writeError( msg: string ): void {
+        this.logger.error( `[ ${ this.file } error ] %c${ msg }` );
+    }
+
+}
