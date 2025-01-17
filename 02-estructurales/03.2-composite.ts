527b395f00102d07719f4893bb2c7ea807bafc20
BattlerHaru <kronk.sdance@gmail.com>
2025-01-17 13:25:47 -0600

    :package: feat: add solution for Composite pattern task in 02-estructurales/03.2-composite.ts

--- 02-estructurales/03.2-composite.ts
+++ 02-estructurales/03.2-composite.ts
@@ -19,7 +19,7 @@ import { COLORS } from '../helpers/colors.ts';
 // 1. Interfaz MenuComponent
 // Define el método `showDetails`, que implementarán los ítems y categorías de menú.
 interface MenuComponent {
-  showDetails(indent?: string): void;
+  showDetails( indent?: string ): void;
 }
 
 // 2. Clase MenuItem
@@ -28,35 +28,43 @@ class MenuItem implements MenuComponent {
   private name: string;
   private price: number;
 
-  constructor(name: string, price: number) {
+  constructor( name: string, price: number ) {
     this.name = name;
     this.price = price;
   }
 
-  showDetails(indent: string = ''): void {
+  showDetails( indent: string = '' ): void {
     console.log(
-      `${indent}- ${this.name}: %c$${this.price.toFixed(2)}`,
+      `${ indent }- ${ this.name }: %c$${ this.price.toFixed( 2 ) }`,
       COLORS.green
     );
   }
 }
 
 // 3. Clase MenuCategory
-// Representa una categoría de menú que puede contener otros ítems o subcategorías.
+// Representa una categoría de menú que puede contener otros ítems o sub categorías.
 class MenuCategory implements MenuComponent {
-  // TODO: Crear dos propiedades privadas: name y items
-  // Name sting y items arreglo de MenuComponent
-  // Name es recibida en el constructor, items se inicializa como un arreglo vacío
+  private name: string;
+  private items: MenuComponent[] = [];
 
-  //TODO: Sobrecarga de operadores - Item puede ser MenuComponent o un arreglo de MenuComponent
-  add(item: unknown): void {
-    // TODO: Implementar la sobrecarga de operadores
-    throw new Error('Method not implemented.');
+  constructor( name: string ) {
+    this.name = name;
   }
 
-  showDetails(indent: string = ''): void {
-    console.log(`%c${indent}+ ${this.name}`, COLORS.blue);
-    // TODO: Implementar foreach
+  add( item: MenuComponent | MenuComponent[] ): void {
+    if ( Array.isArray( item ) ) {
+      this.items.push( ...item );
+      return;
+    }
+    this.items.push( item );
+
+  }
+
+  showDetails( indent: string = '' ): void {
+    console.log( `%c${ indent }+ ${ this.name }`, COLORS.blue );
+    this.items.forEach( item => {
+      item.showDetails( indent + " " );
+    } );
   }
 }
 
@@ -65,37 +73,46 @@ class MenuCategory implements MenuComponent {
 // Deben de ver la gráfica correcta del menú
 function main() {
   // Crear ítems individuales
-  const salad = new MenuItem('Ensalada', 5.99);
-  const soup = new MenuItem('Sopa de tomate', 4.99);
-  const steak = new MenuItem('Bistec', 15.99);
-  const soda = new MenuItem('Refresco', 2.5);
-  const dessert = new MenuItem('Pastel de chocolate', 6.5);
-  const coffee = new MenuItem('Café', 1.99);
+  const salad = new MenuItem( 'Ensalada', 5.99 );
+  const soup = new MenuItem( 'Sopa de tomate', 4.99 );
+  const steak = new MenuItem( 'Bistec', 15.99 );
+  const soda = new MenuItem( 'Refresco', 2.5 );
+  const dessert = new MenuItem( 'Pastel de chocolate', 6.5 );
+  const coffee = new MenuItem( 'Café', 1.99 );
+  const te = new MenuItem( 'Té', 0.99 );
+
 
   // Crear categorías de menú y añadir ítems
-  const appetizers = new MenuCategory('Entradas');
-  appetizers.add(salad);
-  appetizers.add(soup);
+  const appetizers = new MenuCategory( 'Entradas' );
+  appetizers.add( salad );
+  appetizers.add( soup );
 
-  const mainCourse = new MenuCategory('Plato Principal');
-  mainCourse.add(steak);
+  const mainCourse = new MenuCategory( 'Plato Principal' );
+  mainCourse.add( steak );
 
-  const beverages = new MenuCategory('Bebidas');
-  beverages.add(soda);
-  beverages.add(coffee);
+  const beverages = new MenuCategory( 'Bebidas' );
+  const hotBeverages = new MenuCategory( 'Calientes' );
+  const coldBeverages = new MenuCategory( 'Frías' );
 
-  const desserts = new MenuCategory('Postres');
-  desserts.add(dessert);
+  coldBeverages.add( soda );
+
+  hotBeverages.add( coffee );
+  hotBeverages.add( te );
+
+  beverages.add( [ hotBeverages, coldBeverages ] );
+
+  const desserts = new MenuCategory( 'Postres' );
+  desserts.add( dessert );
 
   // Crear un menú principal que contiene todas las categorías
-  const mainMenu = new MenuCategory('Menú Principal');
-  mainMenu.add([appetizers, beverages, desserts, mainCourse]);
+  const mainMenu = new MenuCategory( 'Menú Principal' );
+  mainMenu.add( [ appetizers, beverages, desserts, mainCourse ] );
   // mainMenu.add(mainCourse);
   // mainMenu.add(beverages);
   // mainMenu.add(desserts);
 
   // Mostrar la estructura completa del menú
-  console.log('Menú del Restaurante:');
+  console.log( 'Menú del Restaurante:' );
   mainMenu.showDetails();
 }
 
