8ef4838c43ab7f6324b9780ae8d38991668ca5bf
BattlerHaru <kronk.sdance@gmail.com>
2025-01-17 12:23:59 -0600

    :package: feat: another Bridge pattern example use in 02-estructurales/02.3-bridge.ts

--- 02-estructurales/02.3-bridge.ts
+++ 02-estructurales/02.3-bridge.ts
@@ -9,3 +9,67 @@
  *
  * https://refactoring.guru/es/design-patterns/bridge
  */
+
+
+import { COLORS } from '../helpers/colors.ts';
+
+interface NotificationChannel {
+    send( message: string ): void;
+}
+
+class EmailChannel implements NotificationChannel {
+    send( message: string ): void {
+        console.log( `Enviando correo electrónico: ${ message }` );
+    }
+}
+
+class SMSChannel implements NotificationChannel {
+    send( message: string ): void {
+        console.log( `Enviando SMS: ${ message }` );
+    }
+}
+
+class PushNotificationChannel implements NotificationChannel {
+    send( message: string ): void {
+        console.log( `Enviando Push: ${ message }` );
+    }
+}
+
+abstract class Notification {
+    protected channels: NotificationChannel[];
+
+    constructor( channels: NotificationChannel[] ) {
+        this.channels = channels;
+    }
+
+    abstract addChannel( channel: NotificationChannel ): void;
+    abstract notify( message: string ): void;
+}
+
+
+class AlertNotification extends Notification {
+    override addChannel( channel: NotificationChannel ): void {
+        this.channels.push( channel );
+    }
+    override notify( message: string ): void {
+        console.log( "%cNotificación de alerta", COLORS.red );
+        this.channels.forEach( ( channel ) => channel.send( message ) );
+    }
+
+}
+
+function main() {
+    const channels = [
+        new EmailChannel(),
+        new SMSChannel(),
+        new PushNotificationChannel(),
+        new PushNotificationChannel(),
+        new PushNotificationChannel(),
+        new EmailChannel(),
+        new SMSChannel(),
+    ];
+
+    const alert = new AlertNotification( channels );
+    alert.notify( "Algo paso..." );
+}
+main();
