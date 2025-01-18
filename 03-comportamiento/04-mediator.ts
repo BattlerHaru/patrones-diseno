1dfe34d848eab1c5324e1f82b421f9b1e1e0508c
BattlerHaru <kronk.sdance@gmail.com>
2025-01-18 15:21:56 -0600

    :package: feat: add Mediator pattern example use in 03-comportamiento/04-mediator.ts

--- 03-comportamiento/04-mediator.ts
+++ 03-comportamiento/04-mediator.ts
@@ -9,3 +9,70 @@
  *
  * https://refactoring.guru/es/design-patterns/mediator
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+// mediador
+class ChatRoom {
+    private users: Users[] = [];
+    private title: string;
+
+    constructor( title: string ) {
+        this.title = title;
+    }
+
+    addUser( user: User ) {
+        this.users.push( user );
+    }
+
+    sendMessage( sender: User, message: string ): void {
+        const userToSend = this.users.filter( user => user !== sender );
+        for ( const user of userToSend ) {
+            user.receiveMessage( sender, message );
+        }
+    }
+}
+
+class User {
+    private username: string;
+    private chatRoom: ChatRoom;
+
+    constructor( username: string, chatRoom: ChatRoom ) {
+        this.username = username;
+        this.chatRoom = chatRoom;
+
+        chatRoom.addUser( this );
+    }
+
+    sendMessage( message: string ): void {
+        console.log( `\n\n\n%c${ this.username } envía: %c${ message }`,
+            COLORS.blue,
+            COLORS.white,
+        );
+
+        this.chatRoom.sendMessage( this, message );
+    }
+
+    receiveMessage( sender: User, message: string ): void {
+        console.log( `%c${ this.username } recibe de ${ sender.username }: %c${ message }`,
+            COLORS.blue,
+            COLORS.white,
+        );
+    }
+}
+
+function main() {
+    const chatRoom = new ChatRoom( "Grupo de trabajo" );
+
+    const user1 = new User( "Fernando", chatRoom );
+    const user2 = new User( "Gastón", chatRoom );
+    const user3 = new User( "Mariangel", chatRoom );
+
+    user1.sendMessage( "Hola a todos!" );
+    user2.sendMessage( "Hola Fernando, cómo estás?" );
+    user3.sendMessage( "Hola Fernando, Gastón, cómo están?" );
+
+
+
+}
+main();
