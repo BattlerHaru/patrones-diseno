0f085b0c2248600e91a04df6cdb0cb0ddbe71477
BattlerHaru <kronk.sdance@gmail.com>
2025-01-17 18:27:00 -0600

    :package: feat: add solution for Facade pattern task in 02-estructurales/05.2-facade.ts

--- 02-estructurales/05.2-facade.ts
+++ 02-estructurales/05.2-facade.ts
@@ -20,70 +20,77 @@ import { COLORS } from '../helpers/colors.ts';
 
 class CPU {
   stopOperations(): void {
-    console.log('CPU: Deteniendo operaciones.');
+    console.log( 'CPU: Deteniendo operaciones.' );
   }
 
-  jump(position: number): void {
-    console.log(`CPU: Saltando a la posición de memoria ${position}.`);
+  jump( position: number ): void {
+    console.log( `CPU: Saltando a la posición de memoria ${ position }.` );
   }
 
   execute(): void {
-    console.log('CPU: Ejecutando instrucciones.');
+    console.log( 'CPU: Ejecutando instrucciones.' );
   }
 }
 
 class HardDrive {
-  read(position: number, size: number): string {
+  read( position: number, size: number ): string {
     console.log(
-      `HardDrive: Leyendo ${size} bytes desde la posición ${position}.`
+      `HardDrive: Leyendo ${ size } bytes desde la posición ${ position }.`
     );
     return '001010001010100';
   }
 
   close() {
-    console.log('HardDrive: Deteniendo disco duro.');
+    console.log( 'HardDrive: Deteniendo disco duro.' );
   }
 }
 
 class Memory {
-  load(position: number, data: string): void {
-    console.log(`Memory: Cargando datos en la posición ${position} ${data}.`);
+  load( position: number, data: string ): void {
+    console.log( `Memory: Cargando datos en la posición ${ position } ${ data }.` );
   }
 
   free(): void {
-    console.log('Memory: Liberando memoria.');
+    console.log( 'Memory: Liberando memoria.' );
   }
 }
 
 // 2. Clase Facade - ComputerFacade
 
 class ComputerFacade {
-  // TODO: Agregar los atributos necesarios CPU, Memory y HardDrive
+  private cpu: CPU = new CPU();
+  private hardDrive: HardDrive = new HardDrive();
+  private memory: Memory = new Memory();
 
-  // TODO: Agregar el constructor para instanciar los atributos CPU, Memory y HardDrive
-  constructor() {}
+  constructor(
+    // cpu: CPU,
+    // hardDrive: HardDrive,
+    // memory: Memory,
+  ) {
+    // this.cpu = cpu;
+    // this.hardDrive = hardDrive;
+    // this.memory = memory;
+  }
 
   startComputer(): void {
-    console.log('\n%cIniciando la computadora...', COLORS.cyan);
+    console.log( '\n%cIniciando la computadora...', COLORS.cyan );
 
-    // TODO: ejecutar las operaciones necesarias para encender la computadora
-    // 1. Cargar el sistema operativo en la memoria - memory.load(0, hardDrive.read(0, 1024))
-    // 2. Saltar a la posición de memoria 0 - cpu.jump(0)
-    // 3. Ejecutar las instrucciones del CPU - cpu.execute()
+    this.memory.load( 0, this.hardDrive.read( 0, 1024 ) );
+    this.cpu.jump( 0 );
+    this.cpu.execute();
 
-    console.log('Computadora lista para usar.\n');
+    console.log( 'Computadora lista para usar.\n' );
   }
 
   shutDownComputer(): void {
-    console.log('\n%cApagando la computadora...', COLORS.red);
-    console.log('Cerrando procesos y guardando datos...');
+    console.log( '\n%cApagando la computadora...', COLORS.red );
+    console.log( 'Cerrando procesos y guardando datos...' );
 
-    // TODO: ejecutar las operaciones necesarias para apagar la computadora
-    // 1. Detener las operaciones del CPU - cpu.stopOperations()
-    // 2. Liberar la memoria - memory.free()
-    // 3. Cerrar el disco duro - hardDrive.close()
+    this.cpu.stopOperations();
+    this.memory.free();
+    this.hardDrive.close();
 
-    console.log('Computadora apagada.\n');
+    console.log( 'Computadora apagada.\n' );
   }
 }
 
