f0ff0294a6fc015fcf6c56b716ab3169ea5948fc
BattlerHaru <kronk.sdance@gmail.com>
2025-01-18 13:29:44 -0600

    :package: feat: add Iterator pattern example manual use in 03-comportamiento/03-iterator.ts

--- 03-comportamiento/03-iterator.ts
+++ 03-comportamiento/03-iterator.ts
@@ -8,3 +8,89 @@
  *
  * https://refactoring.guru/es/design-patterns/iterator
  */
+
+interface Iterator<T> {
+    next(): T | null;
+    hasNext(): Boolean;
+    current(): T | null;
+}
+
+class Pokemon {
+    public name: string;
+    public type: string;
+
+    constructor( name: string, type: string ) {
+        this.name = name;
+        this.type = type;
+    }
+}
+
+class PokemonCollection {
+    private pokemons: Pokemon[] = [];
+
+    addPokemon( pokemon: Pokemon ) {
+        this.pokemons.push( pokemon );
+    }
+
+    getPokemonAt( index: number ): Pokemon | null {
+        if ( index >= 0 && index < this.pokemons.length ) {
+            return this.pokemons[ index ];
+        }
+        return null;
+    }
+
+    getLength(): number {
+        return this.pokemons.length;
+    }
+
+    createIterator(): PokemonIterator {
+        return new PokemonIterator( this );
+    }
+}
+
+class PokemonIterator implements Iterator<Pokemon> {
+    private collection: PokemonCollection;
+    private position: number = 0;
+
+    constructor( collection: PokemonCollection ) {
+        this.collection = collection;
+    }
+
+    next(): Pokemon | null {
+        if ( this.hasNext() ) {
+            return this.collection.getPokemonAt( this.position++ );
+        }
+        return null;
+    }
+
+    hasNext(): Boolean {
+        return this.position < this.collection.getLength();
+    }
+
+    current(): Pokemon | null {
+        return this.collection.getPokemonAt( this.position );
+    }
+
+}
+
+function main() {
+    const pokemonDex = new PokemonCollection();
+
+    pokemonDex.addPokemon( new Pokemon( 'Pikachu', 'Eléctrico' ) );
+    pokemonDex.addPokemon( new Pokemon( 'Charmander', 'Fuego' ) );
+    pokemonDex.addPokemon( new Pokemon( 'Squirtle', 'Agua' ) );
+    pokemonDex.addPokemon( new Pokemon( 'Bulbasaur', 'Planta' ) );
+    pokemonDex.addPokemon( new Pokemon( 'Jigglypuff', 'Normal' ) );
+
+
+    const iterator = pokemonDex.createIterator();
+
+    while ( iterator.hasNext() ) {
+        const pokemon = iterator.next();
+        if ( pokemon ) {
+            console.log( `Pokémon: ${ pokemon.name }, Tipo: ${ pokemon.type }` );
+        }
+    }
+
+}
+main();
